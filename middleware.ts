d9b0e0924a46c0881c89e79b2d3e6a7f00de08db
ShalltearFR <Jossuha@hotmail.fr>
2024-02-24 00:09:15 +0100

    secure middleware & charactersList Editor

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,14 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { jwtVerify, JWTPayload } from "jose";
 
-export function middleware(request: NextRequest) {
+async function getTokenInfo(request: NextRequest) {
+  const req = await fetch(`${process.env.WWW}/api/me`);
+  const res = await req.json();
+  return res;
+}
+
+export async function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
 
   // Define paths that are considered public (accessible without a token)
@@ -10,19 +17,38 @@ export function middleware(request: NextRequest) {
   // Get the token from the cookies
   const token = request.cookies.get("token")?.value || "";
 
-  // Redirect logic based on the path and token presence
-  if (isPublicPath && token) {
-    // If trying to access a public path with a token, redirect to the home page
-    return NextResponse.redirect(new URL("/hsr-editor/", request.nextUrl));
-  }
+  try {
+    const tokenSecretUint8Array = new TextEncoder().encode(
+      process.env.TOKEN_SECRET
+    );
 
-  // If trying to access a protected path without a token, redirect to the home page
-  if (!isPublicPath && !token) {
-    return NextResponse.redirect(new URL("/hsr-editor/login", request.nextUrl));
+    let isSecuredToken;
+    if (token) {
+      const { payload } = await jwtVerify(token, tokenSecretUint8Array, {
+        algorithms: ["HS256"], // Specify the algorithm used for signing your tokens
+      });
+      isSecuredToken = payload.id === process.env.SECURED_ID;
+    }
+
+    // Verify the token's signature
+    if (isPublicPath && isSecuredToken) {
+      // If trying to access a public path with a token, redirect to the home page
+      return NextResponse.redirect(new URL("/hsr-editor/", request.nextUrl));
+    }
+
+    // If trying to access a protected path without a token, redirect to the home page
+    if (!isPublicPath && !isSecuredToken) {
+      return NextResponse.redirect(
+        new URL("/hsr-editor/login", request.nextUrl)
+      );
+    }
+  } catch (error) {
+    // If the token verification fails, handle specific errors
+    return NextResponse.redirect(new URL("/", request.nextUrl));
   }
 }
 
 // It specifies the paths for which this middleware should be executed.
 export const config = {
-  matcher: ["/hsr-editor", "/hsr-editor/edit", "/hsr-editor/login"],
+  matcher: ["/hsr-editor/:path*", "/api/character"],
 };
