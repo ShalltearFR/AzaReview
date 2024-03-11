2dd41d0dbf8f4317acd162975a0123041803f536
ShalltearFR <Jossuha@hotmail.fr>
2024-03-12 00:35:16 +0100

    light refacto

--- components/Front/Character/CharacterRelicsSet.tsx
+++ components/Front/Character/CharacterRelicsSet.tsx
@@ -221,35 +221,29 @@ const CharacterRelicsSet: React.FC<CharacterRelicsSetProps> = ({
                   relicsList.includes(relicAlt.id)
                 );
 
-                if (i === 2) {
-                  relicsMap = ornamantsAltList;
-                  description = "Ornements possibles :";
-                } else if (
-                  i === 1 &&
-                  asOrnament.relicsNumber &&
-                  asOrnament.relicsNumber[0] === 4 &&
-                  asOrnament.isGood
+                if (
+                  i === 2 ||
+                  (i === 1 &&
+                    asOrnament.relicsNumber &&
+                    asOrnament.relicsNumber[0] === 4 &&
+                    asOrnament.isGood) ||
+                  (i === 1 &&
+                    asOrnament.relicsNumber &&
+                    asOrnament.relicsNumber[0] === 4) ||
+                  (i === 0 &&
+                    asOrnament.relicsNumber &&
+                    asOrnament.relicsNumber[0] === 2 &&
+                    asOrnament.asAnOrnamant)
                 ) {
-                  relicsMap = ornamantsAltList;
-                  description = "Ornements possibles :";
-                } else if (
-                  i === 1 &&
-                  asOrnament.relicsNumber &&
-                  asOrnament.relicsNumber[0] === 4
-                ) {
-                  relicsMap = ornamantsAltList;
-                  description = "Ornements possibles :";
-                } else if (
-                  i === 0 &&
-                  asOrnament.relicsNumber &&
-                  asOrnament.relicsNumber[0] === 2 &&
-                  asOrnament.asAnOrnamant === true
-                ) {
-                  relicsMap = ornamantsAltList;
-                  description = "Ornements possibles :";
+                  return {
+                    relicsMap: ornamantsAltList,
+                    description: "Ornements possibles :",
+                  };
                 } else {
-                  relicsMap = relicsAltList;
-                  description = "Reliques possibles :";
+                  return {
+                    relicsMap: relicsAltList,
+                    description: "Reliques possibles :",
+                  };
                 }
               }
 
