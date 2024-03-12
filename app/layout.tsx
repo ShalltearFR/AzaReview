e56a0a455f3c227b07af14696716814e0b1f594f
ShalltearFR <Jossuha@hotmail.fr>
2024-03-12 17:35:00 +0100

    add metaData in homepage

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,15 @@ import "./globals.css";
 import Footer from "@/components/Front/Footer";
 import type { Metadata } from "next";
 import NavBar from "@/components/Front/NavBar";
+import { CDN2 } from "@/utils/cdn";
 
 export const metadata: Metadata = {
+  metadataBase: new URL(CDN2),
   title: "Review HSR",
-  description: "...",
+  description: "Votre review Honkai : Star Rail, rapidement et simplement",
+  openGraph: {
+    images: [`/img/logo_SRE.webp`],
+  },
 };
 
 export default function RootLayout({
