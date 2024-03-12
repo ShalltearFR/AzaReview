8ff69207fa9edd26605697fd0e820bf00eea5f7a
ShalltearFR <Jossuha@hotmail.fr>
2024-03-12 13:11:03 +0100

    replace stars

--- app/page.tsx
+++ app/page.tsx
@@ -46,59 +46,59 @@ export default function App() {
 
         <ParallaxLayer offset={1} speed={0.8} style={{ opacity: 0.1 }}>
           <img
-            src={url("stars.png", true)}
+            src={url("stars.png")}
             style={{ display: "block", width: "20%", marginLeft: "55%" }}
           />
           <img
-            src={url("stars.png", true)}
+            src={url("stars.png")}
             style={{ display: "block", width: "10%", marginLeft: "15%" }}
           />
         </ParallaxLayer>
 
         <ParallaxLayer offset={1.75} speed={0.5} style={{ opacity: 0.1 }}>
           <img
-            src={url("stars.png", true)}
+            src={url("stars.png")}
             style={{ display: "block", width: "20%", marginLeft: "70%" }}
           />
           <img
-            src={url("stars.png", true)}
+            src={url("stars.png")}
             style={{ display: "block", width: "20%", marginLeft: "40%" }}
           />
         </ParallaxLayer>
 
         <ParallaxLayer offset={1} speed={0.2} style={{ opacity: 0.2 }}>
           <img
-            src={url("stars.png", true)}
+            src={url("stars.png")}
             style={{ display: "block", width: "10%", marginLeft: "10%" }}
           />
           <img
-            src={url("stars.png", true)}
+            src={url("stars.png")}
             style={{ display: "block", width: "20%", marginLeft: "75%" }}
           />
         </ParallaxLayer>
 
         <ParallaxLayer offset={1.6} speed={-0.1} style={{ opacity: 0.4 }}>
           <img
-            src={url("stars.png", true)}
+            src={url("stars.png")}
             style={{ display: "block", width: "20%", marginLeft: "60%" }}
           />
           <img
-            src={url("stars.png", true)}
+            src={url("stars.png")}
             style={{ display: "block", width: "25%", marginLeft: "30%" }}
           />
           <img
-            src={url("stars.png", true)}
+            src={url("stars.png")}
             style={{ display: "block", width: "10%", marginLeft: "80%" }}
           />
         </ParallaxLayer>
 
         <ParallaxLayer offset={2.6} speed={0.4} style={{ opacity: 0.6 }}>
           <img
-            src={url("stars.png", true)}
+            src={url("stars.png")}
             style={{ display: "block", width: "20%", marginLeft: "5%" }}
           />
           <img
-            src={url("stars.png", true)}
+            src={url("stars.png")}
             style={{ display: "block", width: "15%", marginLeft: "75%" }}
           />
         </ParallaxLayer>
