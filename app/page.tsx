55b43a295f523b9a40b6fc2476690dfec592048f
ShalltearFR <Jossuha@hotmail.fr>
2024-03-18 13:43:06 +0100

    add /?codes responsive in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -4,19 +4,24 @@ import HorizontalNotationArray from "@/components/Front/Homepage/HorizontalNotat
 import VerticalNotationArray from "@/components/Front/Homepage/VerticalNotationArray";
 import NavBar from "@/components/Front/NavBar";
 import { CDN2 } from "@/utils/cdn";
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import AOS from "aos";
+import { useSearchParams } from "next/navigation";
+import Footer from "@/components/Front/Footer";
 
 export default function App() {
-  const [sectionIndex, setSectionIndex] = useState<number>(0);
+  const [sectionIndex, setSectionIndex] = useState<number>(999);
   const [sectionPrevIndex, setSectionPrevIndex] = useState<number>(0);
   const [codes, setCodes] = useState<Array<string>>([""]);
-  const desktopMode = useRef(false);
+  const [isCodeAnimation, setIsCodeAnimation] = useState<Boolean>(true);
+  const searchParams = useSearchParams();
+  const isCodes = useRef(false);
+  const [isLoading, setIsloading] = useState<boolean>(false);
 
   useEffect(() => {
     AOS.init({ mirror: true });
     window.addEventListener("resize", userResizing, true);
-    if (window.innerWidth <= 1700) setSectionIndex(999);
+    if (window.innerWidth >= 1700) setSectionIndex(0);
 
     fetch("/api/other/all")
       .then((res) => res.json())
@@ -30,20 +35,89 @@ export default function App() {
         }
       });
 
+    const codes = searchParams.get("codes");
+    // Si l'url possède ?codes
+    if (codes !== null) {
+      isCodes.current = true;
+      if (window.innerWidth >= 1700) {
+        // Version desktop
+        setSectionPrevIndex(1);
+        setSectionIndex(2);
+      }
+    }
+    setIsloading(true);
     //eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  useEffect(() => {
+    if (isCodes.current && window.innerWidth <= 1700) {
+      setIsCodeAnimation(false);
+      setTimeout(() => {
+        const codesEl: any = document.querySelector("#codes");
+        codesEl.scrollIntoView({ behavior: "auto" });
+      }, 1000);
+    }
+  }, [isLoading]);
+
   const userResizing = () => {
-    if (window.innerWidth <= 1700) setSectionIndex(999);
-    if (window.innerWidth > 1700) setSectionIndex(0);
     const startPage: any = document.querySelector("body");
-    startPage.scrollIntoView({ behavior: "auto" });
+    if (window.innerWidth <= 1700) {
+      setSectionIndex(999);
+      if (isCodes.current) {
+        const codesEl: any = document.querySelector("#codes");
+        codesEl.scrollIntoView({ behavior: "auto" });
+      } else {
+        startPage.scrollIntoView({ behavior: "auto" });
+      }
+    } else {
+      if (isCodes.current) {
+        setSectionIndex(2);
+      } else {
+        setSectionIndex(0);
+        startPage.scrollIntoView({ behavior: "auto" });
+      }
+    }
   };
 
   const showSection0 = sectionIndex === 0 || sectionIndex === 999;
   const showSection1 = sectionIndex === 1 || sectionIndex === 999;
   const showSection2 = sectionIndex === 2 || sectionIndex === 999;
 
+  if (!isLoading) {
+    return (
+      <>
+        <div
+          style={{
+            backgroundImage: `url("${CDN2}/img/homepage/stars.svg")`,
+            zIndex: 0,
+          }}
+          data-aos="animate-stars"
+        />
+        <NavBar />
+        <div className="flex w-screen h-[calc(100vh-290px)] justify-center items-center">
+          <div role="status">
+            <svg
+              aria-hidden="true"
+              className="w-24 h-24 text-gray animate-spin  fill-orange"
+              viewBox="0 0 100 101"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="currentColor"
+              />
+              <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentFill"
+              />
+            </svg>
+          </div>
+        </div>
+        <Footer />
+      </>
+    );
+  }
   return (
     <>
       <div
@@ -284,10 +358,13 @@ export default function App() {
             >
               <div className="flex flex-col-reverse mb-16 xl2:mb-0 xl2:flex-row gap-5 justify-center items-center my-auto">
                 <div
-                  data-aos="fade-right"
+                  data-aos={isCodeAnimation ? "fade-right" : ""}
                   className="bg-black mmd:rounded-2xl w-full mmd:w-[600px] p-6 z-20 xl2:mt-36"
                 >
-                  <h2 className="text-3xl font-bold text-center mb-5">
+                  <h2
+                    id="codes"
+                    className="text-3xl font-bold text-center mb-5"
+                  >
                     Codes actifs
                   </h2>
                   {codes.length > 1 ? (
