e22fefa6d02c3129fbea0dd33a019ad11349ffe4
ShalltearFR <Jossuha@hotmail.fr>
2024-03-21 13:52:30 +0100

    fix changelog page

--- app/changelog/page.tsx
+++ app/changelog/page.tsx
@@ -11,29 +11,34 @@ const getData = async (url: string) => {
 const Changelog: React.FC = async () => {
   const json: CommitData = await getData(`${process.env.WWW}/api/changelog`);
 
-  return (
-    <div className="bg-white overflow-hidden">
-      {json.data.map((el) => {
-        const date = new Date(el.date);
-        const day = date.getDate();
-        const month = date.toLocaleString("en-US", { month: "long" });
-        const formattedDate = `${day} ${month}`;
+  if (json) {
+    return (
+      <div className="bg-white overflow-hidden">
+        {json &&
+          json.data.map((el) => {
+            const date = new Date(el.date);
+            const day = date.getDate();
+            const month = date.toLocaleString("en-US", { month: "long" });
+            const formattedDate = `${day} ${month}`;
 
-        return (
-          <div key={el.date} className="mb-5">
-            <p>{formattedDate}</p>
-            <ul className="list-disc">
-              {el.message.map((message, i) => (
-                <li className="ml-5" key={`${message}+${i}`}>
-                  {message}
-                </li>
-              ))}
-            </ul>
-          </div>
-        );
-      })}
-    </div>
-  );
+            return (
+              <div key={el.date} className="mb-5">
+                <p>{formattedDate}</p>
+                <ul className="list-disc">
+                  {el.message.map((message, i) => (
+                    <li className="ml-5" key={`${message}+${i}`}>
+                      {message}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            );
+          })}
+      </div>
+    );
+  }
+
+  return <div></div>;
 };
 
 export default Changelog;
