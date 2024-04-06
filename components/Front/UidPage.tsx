1a286ac09b693231ae0de01ae317cedb07d00adf
ShalltearFR <Jossuha@hotmail.fr>
2024-04-06 17:07:27 +0200

    add button download & share with chromium

--- components/Front/UidPage.tsx
+++ components/Front/UidPage.tsx
@@ -5,8 +5,8 @@ import NavBar from "@/components/Front/NavBar";
 import { CharacterType } from "@/types/CharacterModel";
 import type { jsonUID } from "@/types/jsonUid";
 import { useSearchParams } from "next/navigation";
-import { useState, useEffect, useRef, useCallback } from "react";
-import { toBlob, toCanvas, toJpeg, toPng, toSvg } from "html-to-image";
+import { useState, useEffect, useRef } from "react";
+import { toBlob, toJpeg, toPng, toSvg } from "html-to-image";
 import ReactSelect from "react-select";
 import { CDN, CDN2 } from "@/utils/cdn";
 import Aos from "aos";
@@ -56,11 +56,17 @@ const UidPage: React.FC<UidPageProps> = ({
   ]);
 
   const characterDetailsRef = useRef<HTMLDivElement>(null);
-  const reviewRef = useRef<HTMLDivElement>(null);
+  const reviewHeaderRef = useRef<any>(null);
   const [exportImg, setExportImg] = useState<any>("");
-  const [disableButton, setDisableButton] = useState<boolean>(false);
+  const [disableDownloadButton, setDisableDownloadButton] =
+    useState<boolean>(false);
+  const [disableShareButton, setDisableShareButton] = useState<boolean>(false);
+  const [shareButtonText, setShareButtonText] = useState<string>(
+    "Sauvegarde en cours..."
+  );
 
   const [review, setReview] = useState<any>();
+  const isFirefox = navigator.userAgent.toLowerCase().includes("firefox");
 
   useEffect(() => {
     Aos.init({ disable: window.innerWidth <= 1450 });
@@ -84,96 +90,110 @@ const UidPage: React.FC<UidPageProps> = ({
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const htmlToImageConvert = useCallback(() => {
-    setDisableButton(true);
+  const handleConvertImage = (
+    exportType: string,
+    disableButton: (value: boolean) => void
+  ) => {
+    disableButton(true);
     if (characterDetailsRef.current === null) {
       return;
     }
 
-    toPng(characterDetailsRef.current)
-      .then((dataUrl) => {
-        // Generation du Header et impression du CharactersDetails
-        console.log("dataUrl", dataUrl);
-        const reviewDiv = (
-          <div id="exportedImage">
-            <div className="w-full max-w-[1450px] mx-auto">
-              <div
-                id="exportHTML"
-                className="mx-auto w-full max-w-[1450px] border rounded-t-xl overflow-hidden"
-                style={{
-                  backgroundImage: `url('${CDN2}/img/character_bg.avif')`,
-                  marginBlock: "auto",
-                  width: "100%",
-                  height: "96px",
-                }}
-              >
-                <div className="flex items-center w-full h-24 text-white smd:text-xl bg-black/50">
-                  <img
-                    className="ml-2 smd:ml-5 h-20"
-                    src={`${CDN}/${jsonUid?.player?.avatar.icon}`}
-                  />
-                  <p className="ml-5 flex flex-col">
-                    <span className="font-bold">
-                      {jsonUid?.player?.nickname}
-                    </span>
-                    <span className="smd:text-base">
-                      UID : {jsonUid?.player?.uid}
-                    </span>
-                  </p>
-                  <img
-                    src={`${CDN2}/img/homepage/logo_min.png`}
-                    className="ml-auto hidden smd:block mr-2 smd:mr-5 h-10 smd:h-14"
-                  />
-                  <p className="ml-auto smd:ml-0 mr-5 font-bold">
-                    review-hsr.vercel.app
-                  </p>
-                </div>
-              </div>
-              <img src={dataUrl} />
-            </div>
-          </div>
+    setTimeout(() => {
+      //Leger timeout pour eviter le tronc d'image du au rajout du header Image
+      let conversionPromise;
+
+      if (isFirefox) {
+        conversionPromise = toSvg(
+          characterDetailsRef.current as HTMLDivElement
         );
-        setExportImg(reviewDiv);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [characterDetailsRef]);
+      } else if (window.innerWidth >= 650) {
+        // Tablette/Desktop
+        conversionPromise = toPng(
+          characterDetailsRef.current as HTMLDivElement
+        );
+      } else {
+        // Mobile
+        conversionPromise = toJpeg(
+          characterDetailsRef.current as HTMLDivElement,
+          {
+            quality: 0.5,
+          }
+        );
+      }
+
+      conversionPromise
+        .then((dataImage) => {
+          if (isFirefox) {
+            setExportImg(<img src={dataImage} />);
+            disableButton(false);
+            return;
+          }
+
+          if (exportType === "save") {
+            // Bouton sauvegarder
+            const link = document.createElement("a");
+            link.download = `Review HSR - ${jsonUid.characters[characterIndex].name}`;
+            link.href = dataImage;
+            link.click();
+            disableButton(false);
+          } else if (exportType === "share") {
+            // Bouton Copier Presse Papier
+            toBlob(characterDetailsRef.current as HTMLDivElement).then(
+              (blob: any) => {
+                navigator.clipboard
+                  .write([
+                    new ClipboardItem({
+                      "image/png": blob,
+                    }),
+                  ])
+                  .then(() => {
+                    setShareButtonText("Image copiée avec succès");
+                    setTimeout(() => disableButton(false), 1000);
+                  })
+                  .catch((err) => console.log("Erreur presse papier :", err));
+              }
+            );
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }, 1000);
+  };
 
   useEffect(() => {
-    // Generation d'image finale header + CharacterDetails
-    if (reviewRef.current !== null && disableButton) {
-      if (window.innerWidth <= 650) {
-        // Mobile
-        toJpeg(reviewRef.current, {
-          cacheBust: false,
-          quality: window.innerWidth >= 650 ? 1 : 0.5,
-        })
-          .then((image) => {
-            const link = document.createElement("a");
-            link.download = `ReviewHSR ${jsonUid.characters[characterIndex].name}`;
-            link.href = image;
-            link.click();
-            setExportImg(<img src={image} />);
-            setDisableButton(false);
-          })
-          .catch((err) => console.log("erreur image", err));
-      } else {
-        // Tablette/PC
-        toPng(reviewRef.current, {
-          cacheBust: false,
-        })
-          .then((image) => {
-            console.log("image", image);
-            setExportImg(<img src={image} />);
-            setDisableButton(false);
-          })
-          .catch((err) => console.log("erreur image", err));
-      }
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [exportImg]);
+    reviewHeaderRef.current = (
+      <div
+        id="exportHTML"
+        className="mx-auto w-full max-w-[1450px] border rounded-t-xl overflow-hidden"
+        style={{
+          backgroundImage: `url('${CDN2}/img/character_bg.avif')`,
+          marginBlock: "auto",
+          width: "100%",
+          height: "96px",
+        }}
+      >
+        <div className="flex items-center w-full h-24 text-white smd:text-xl bg-black/50">
+          <img
+            className="ml-2 smd:ml-5 h-20"
+            src={`${CDN}/${jsonUid?.player?.avatar.icon}`}
+          />
+          <p className="ml-5 flex flex-col">
+            <span className="font-bold">{jsonUid?.player?.nickname}</span>
+            <span className="smd:text-base">UID : {jsonUid?.player?.uid}</span>
+          </p>
+          <img
+            src={`${CDN2}/img/homepage/logo_min.png`}
+            className="ml-auto hidden smd:block mr-2 smd:mr-5 h-10 smd:h-14"
+          />
+          <p className="ml-auto smd:ml-0 mr-5 font-bold">
+            review-hsr.vercel.app
+          </p>
+        </div>
+      </div>
+    );
+  }, [jsonUid]);
 
   useEffect(() => {
     function sortReviewDataByUidData(reviewData: any, uidData: any) {
@@ -349,7 +369,13 @@ const UidPage: React.FC<UidPageProps> = ({
             </div>
           </div>
           <div className="flex justify-center w-full">
-            <div ref={characterDetailsRef} className="w-full max-w-[1450px]">
+            <div
+              ref={characterDetailsRef}
+              className="w-full max-w-[1450px] h-full"
+            >
+              {disableDownloadButton || disableShareButton
+                ? reviewHeaderRef.current
+                : null}
               <CharacterDetails
                 uidData={uidData as jsonUID}
                 buildIndex={characterBuild}
@@ -363,47 +389,83 @@ const UidPage: React.FC<UidPageProps> = ({
             </div>
           </div>
           <div>
-            <button
-              disabled={disableButton}
-              className="flex px-5 py-2 mt-10 rounded-full bg-green mx-auto text-xl font-bold mb-10 xl:mb-0 disabled:bg-gray"
-              onClick={htmlToImageConvert}
-            >
-              {disableButton ? (
-                <div className="flex gap-2">
-                  <span>Exportation en cours...</span>
-                  <div role="status">
-                    <svg
-                      aria-hidden="true"
-                      className="w-6 h-6 text-gray animate-spin  fill-orange"
-                      viewBox="0 0 100 101"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                        fill="currentColor"
-                      />
-                      <path
-                        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                        fill="currentFill"
-                      />
-                    </svg>
-                  </div>
-                </div>
-              ) : (
-                "Exporter l'image"
+            <div className="flex justify-center gap-10">
+              {!isFirefox && (
+                <button
+                  disabled={disableDownloadButton}
+                  className="flex px-5 py-2 mt-10 rounded-full bg-green text-xl font-bold mb-10 xl:mb-0 disabled:bg-gray"
+                  onClick={() =>
+                    handleConvertImage("save", setDisableDownloadButton)
+                  }
+                >
+                  {disableDownloadButton ? (
+                    <div className="flex gap-2">
+                      <span>Telechargement en cours...</span>
+                      <div role="status">
+                        <svg
+                          aria-hidden="true"
+                          className="w-6 h-6 text-gray animate-spin  fill-orange"
+                          viewBox="0 0 100 101"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                            fill="currentColor"
+                          />
+                          <path
+                            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                            fill="currentFill"
+                          />
+                        </svg>
+                      </div>
+                    </div>
+                  ) : (
+                    "Telecharger l'image"
+                  )}
+                </button>
               )}
-            </button>
 
-            <div className="mt-10 mx-auto flex justify-center">
-              <div
-                className="w-full max-w-[1450px]"
-                id="exportEEEEEEEED"
-                ref={reviewRef}
+              <button
+                disabled={disableShareButton}
+                className="flex px-5 py-2 mt-10 rounded-full bg-green text-xl font-bold mb-10 xl:mb-0 disabled:bg-gray"
+                onClick={() =>
+                  handleConvertImage("share", setDisableShareButton)
+                }
               >
-                {exportImg && <div>{exportImg}</div>}
-              </div>
+                {disableShareButton ? (
+                  <div className="flex gap-2">
+                    <span>{shareButtonText}</span>
+                    <div role="status">
+                      <svg
+                        aria-hidden="true"
+                        className="w-6 h-6 text-gray animate-spin  fill-orange"
+                        viewBox="0 0 100 101"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                          fill="currentColor"
+                        />
+                        <path
+                          d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                          fill="currentFill"
+                        />
+                      </svg>
+                    </div>
+                  </div>
+                ) : (
+                  "Partager"
+                )}
+              </button>
             </div>
+
+            {exportImg && (
+              <div className="mt-10 mx-auto flex justify-center">
+                <div className="w-full max-w-[1450px]">{exportImg}</div>
+              </div>
+            )}
           </div>
         </section>
       </div>
