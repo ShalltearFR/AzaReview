3417dd55d7d5bee06321b81e6ad5fdc06b6e2121
ShalltearFR <Jossuha@hotmail.fr>
2024-05-12 00:05:11 +0200

    change {NICKNAME} by pioneer

--- components/Front/Showcase/CharactersList.tsx
+++ components/Front/Showcase/CharactersList.tsx
@@ -1,12 +1,14 @@
 import { CharacterType } from "@/types/CharacterModel";
+import { PioneerType } from "@/utils/PioneerType";
 import { CDN } from "@/utils/cdn";
 import Link from "next/link";
 
 interface GuidesProps {
   list: CharacterType[];
+  lang: string | undefined;
 }
 
-const CharactersList: React.FC<GuidesProps> = ({ list }) => {
+const CharactersList: React.FC<GuidesProps> = ({ list, lang }) => {
   return (
     <div className="flex flex-wrap gap-5 justify-center my-10">
       {list.map((character) => (
@@ -16,7 +18,11 @@ const CharactersList: React.FC<GuidesProps> = ({ list }) => {
           className="w-36 sm:w-52 bg-black/75 rounded-3xl relative hover:bg-black/50"
         >
           <p className="absolute bottom-0 text-center text-white font-bold bg-black w-full h-10 flex items-center justify-center">
-            {character.name}
+            {PioneerType.some((item) => item.id === character.id)
+              ? lang === "en"
+                ? PioneerType.find((item) => item.id === character.id)?.nameEN
+                : PioneerType.find((item) => item.id === character.id)?.nameFR
+              : character.name}
           </p>
           <img src={`${CDN}/${character.preview}`} />
         </Link>
