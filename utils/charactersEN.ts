2adf05c73dd65791c23ac3f25ad4f9e6286da6fb
ShalltearFR <Jossuha@hotmail.fr>
2024-06-16 14:12:23 +0200

    add Arlan translation

--- utils/charactersEN.ts
+++ utils/charactersEN.ts
@@ -434,6 +434,15 @@ const characterEN: CharacterENProps = {
       comment: "",
     },
   ],
+  1008: [
+    // Arlan
+    {
+      name: "Saving soldier Arlan",
+      desc: "Arlan deals [color=#DB76ED]Thunder[/color] damage and consumes his life instead of Skill Points.\nThe less life he has, the more damage he deals.",
+      comment:
+        "If Glamoth Ornaments ➜ [u]Speed[/u] at 135.\nIf different Relics from [u]Everlasting Disciple[/u] ➜ [u]Crit Chance[/u] at 70%.",
+    },
+  ],
   1315: [
     // Boothill
     {
