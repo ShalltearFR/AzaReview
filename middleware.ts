83c80f6c187fcb73ad0a95d7422cf8fc46485e1b
ShalltearFR <Jossuha@hotmail.fr>
2024-06-16 11:02:10 +0200

    remove comment

--- middleware.ts
+++ middleware.ts
@@ -4,11 +4,7 @@ import { jwtVerify } from "jose";
 
 export async function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
-
-  // Define paths that are considered public (accessible without a token)
   const isPublicPath = path === "/hsr-editor/login";
-
-  // Get the token from the cookies
   const token = request.cookies.get("token")?.value || "";
 
   try {
@@ -19,30 +15,25 @@ export async function middleware(request: NextRequest) {
     let isSecuredToken;
     if (token) {
       const { payload } = await jwtVerify(token, tokenSecretUint8Array, {
-        algorithms: ["HS256"], // Specify the algorithm used for signing your tokens
+        algorithms: ["HS256"],
       });
       isSecuredToken = payload.id === process.env.SECURED_ID;
     }
 
-    // Verify the token's signature
     if (isPublicPath && isSecuredToken) {
-      // If trying to access a public path with a token, redirect to the home page
       return NextResponse.redirect(new URL("/hsr-editor/", request.nextUrl));
     }
 
-    // If trying to access a protected path without a token, redirect to the home page
     if (!isPublicPath && !isSecuredToken) {
       return NextResponse.redirect(
         new URL("/hsr-editor/login", request.nextUrl)
       );
     }
   } catch (error) {
-    // If the token verification fails, handle specific errors
     return NextResponse.redirect(new URL("/", request.nextUrl));
   }
 }
 
-// It specifies the paths for which this middleware should be executed.
 export const config = {
   matcher: [
     "/hsr-editor/:path*",
