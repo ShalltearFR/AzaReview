82228795b3a0d1330a9bb21370db04ce3ff9e3d7
ShalltearFR <Jossuha@hotmail.fr>
2024-06-20 12:04:29 +0200

    correct query-input

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
     potentialAction: {
       "@type": "SearchAction",
       target: "https://review-hsr.vercel.app/uid/{search_term_string}",
-      "query-input": "Entrez votre UID Honkai : Star Rail",
+      "query-input": "required name=search_term_string",
     },
   };
 
