b2efe5928c7abc1bd8f8e4679c39e6b1bd42d25e
ShalltearFR <Jossuha@hotmail.fr>
2024-10-23 22:17:46 +0200

    fix crash if recent character in db

--- components/Front/Showcase/ItemShow.tsx
+++ components/Front/Showcase/ItemShow.tsx
@@ -45,65 +45,68 @@ const ItemShow: React.FC<ItemShowProps> = ({
     }
   }, [id, lightconesRanks]);
 
-  return (
-    <div className="relative">
-      {relicSet && (
-        <p
-          className={`bg-gray flex justify-center items-center rounded-full absolute -top-2 -left-2 text-black font-semibold text-lg z-10 h-8 ${
-            relicSet.num === 2.4 ? "w-12" : "w-8"
-          }`}
-        >
-          {relicSet.num === 2.4 ? "2/4P" : `${relicSet.num}P`}
-        </p>
-      )}
-      <div
-        className={`${
-          showTooltipName ? "bg-white/5" : "bg-black/75"
-        } rounded-tr-3xl py-2 relative overflow-hidden ${
-          className ? className : ""
-        }`}
-        onMouseEnter={() => setShowTooltipName(true)}
-        onMouseLeave={() => setShowTooltipName(false)}
-      >
-        {showTooltipName && (
-          <p className="absolute bottom-0 bg-black z-20 w-full px-1 text-center">
-            {itemName.name}
+  if (itemName)
+    return (
+      <div className="relative">
+        {relicSet && (
+          <p
+            className={`bg-gray flex justify-center items-center rounded-full absolute -top-2 -left-2 text-black font-semibold text-lg z-10 h-8 ${
+              relicSet.num === 2.4 ? "w-12" : "w-8"
+            }`}
+          >
+            {relicSet.num === 2.4 ? "2/4P" : `${relicSet.num}P`}
           </p>
         )}
-        <img
-          src={`${CDN}/${itemName.icon}`}
-          alt={itemName.name}
-          className="m-auto"
-          width={128}
-          height={128}
-        />
-        <p className="absolute bottom-0 px-1 text-center w-full bg-black/75">
-          {itemName.name.length > 14
-            ? `${itemName.name.substring(0, 14)}...`
-            : itemName.name}
-        </p>
-        <button
-          className="absolute top-0 right-0 size-10"
-          onMouseEnter={() => setShowTooltipDesc(true)}
-          onMouseLeave={() => setShowTooltipDesc(false)}
+        <div
+          className={`${
+            showTooltipName ? "bg-white/5" : "bg-black/75"
+          } rounded-tr-3xl py-2 relative overflow-hidden ${
+            className ? className : ""
+          }`}
+          onMouseEnter={() => setShowTooltipName(true)}
+          onMouseLeave={() => setShowTooltipName(false)}
         >
-          <InformationCircleIcon
-            color="black"
-            fill="gray"
-            className="hidden xl:block"
+          {showTooltipName && (
+            <p className="absolute bottom-0 bg-black z-20 w-full px-1 text-center">
+              {itemName.name}
+            </p>
+          )}
+          <img
+            src={`${CDN}/${itemName.icon}`}
+            alt={itemName.name}
+            className="m-auto"
+            width={128}
+            height={128}
           />
-        </button>
-      </div>
-      {/* Affiche la description de l'item */}
-      {showTooltipDesc && (
-        <div className="absolute bg-background -left-32 border border-orange rounded-xl z-20 p-2 hidden xl:flex xl:flex-col xl:w-[400px] xl:gap-3">
-          {itemDesc.map((desc: string) => (
-            <p key={`desc${id}`}>{desc}</p>
-          ))}
+          <p className="absolute bottom-0 px-1 text-center w-full bg-black/75">
+            {itemName.name.length > 14
+              ? `${itemName.name.substring(0, 14)}...`
+              : itemName.name}
+          </p>
+          <button
+            className="absolute top-0 right-0 size-10"
+            onMouseEnter={() => setShowTooltipDesc(true)}
+            onMouseLeave={() => setShowTooltipDesc(false)}
+          >
+            <InformationCircleIcon
+              color="black"
+              fill="gray"
+              className="hidden xl:block"
+            />
+          </button>
         </div>
-      )}
-    </div>
-  );
+        {/* Affiche la description de l'item */}
+        {showTooltipDesc && (
+          <div className="absolute bg-background -left-32 border border-orange rounded-xl z-20 p-2 hidden xl:flex xl:flex-col xl:w-[400px] xl:gap-3">
+            {itemDesc.map((desc: string) => (
+              <p key={`desc${id}`}>{desc}</p>
+            ))}
+          </div>
+        )}
+      </div>
+    );
+
+  return null;
 };
 
 export default ItemShow;
