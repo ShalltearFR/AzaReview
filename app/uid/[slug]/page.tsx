7e3530bf954db9cb2419ae201d2aee8492bb0302
ShalltearFR <Jossuha@hotmail.fr>
2024-10-29 19:58:32 +0100

    static json and optimise sharedUID

--- app/uid/[slug]/page.tsx
+++ app/uid/[slug]/page.tsx
@@ -6,10 +6,16 @@ import { CDN } from "@/utils/cdn";
 import shareCharactersStats from "@/utils/shareCharactersStats";
 import type { Metadata } from "next";
 import { cookies } from "next/headers";
-
-type Props = {
-  params: { slug: number };
-};
+import relic_setsFR from "@/static/relic_setsFR.json";
+import relic_setsEN from "@/static/relic_setsEN.json";
+import light_conesFR from "@/static/light_conesFR.json";
+import light_conesEN from "@/static/light_conesEN.json";
+import character_ranksFR from "@/static/character_ranksFR.json";
+import character_ranksEN from "@/static/character_ranksEN.json";
+import relicsFR from "@/static/relicsFR.json";
+import relicsEN from "@/static/relicsEN.json";
+import propertiesEN from "@/static/propertiesEN.json";
+import propertiesFR from "@/static/propertiesFR.json";
 
 async function getData(
   url: string,
@@ -38,8 +44,6 @@ async function getDataUid(uid: number, lang: string | undefined) {
       next: { revalidate: 300 },
     }
   );
-
-  console.log(data);
 
   const jsonData = await data.json();
   // const status = 504; // Simule une erreur 504 pour les tests
@@ -91,6 +95,17 @@ export async function generateMetadata({
   };
 }
 
+const sharedUID: string[] = [];
+function isAllreadyShared(uid: string) {
+  if (sharedUID.includes(uid)) return true;
+  return false;
+}
+
+// Remise à zéro de la liste des UID partagés toutes les 30 minutes
+setInterval(() => {
+  sharedUID.length = 0;
+}, 1000 * 60 * 30);
+
 export default async function Page({
   params,
 }: {
@@ -103,33 +118,30 @@ export default async function Page({
   //Recupère les infos du joueur
   const resUid = await getDataUid(slug, lang);
   const jsonUid: jsonUID = await resUid.json();
+
   // Partage les stats des personnages
-  if (jsonUid.characters)
+  if (jsonUid.characters && !isAllreadyShared(jsonUid.player.uid)) {
     jsonUid.characters.map((character: Character) =>
       shareCharactersStats(character, jsonUid.player.uid)
     );
+    sharedUID.push(jsonUid.player.uid);
+  }
 
-  const [
-    resReview,
-    statsTranslate,
-    relicsSetTranslate,
-    lightconesTranslate,
-    relicsList,
-    eidolonsList,
-    changelog,
-  ] = await Promise.all([
+  const [resReview, changelog] = await Promise.all([
     getData(`${process.env.WWW}/api/characters/all`, 5, false),
-    getData(`${CDN}/index_min/${lang || "fr"}/properties.json`, 86400, true),
-    getData(`${CDN}/index_min/${lang || "fr"}/relic_sets.json`, 18000, true),
-    getData(`${CDN}/index_min/${lang || "fr"}/light_cones.json`, 18000, true),
-    getData(`${CDN}/index_min/${lang || "fr"}/relics.json`, 18000, true),
-    getData(
-      `${CDN}/index_min/${lang || "fr"}/character_ranks.json`,
-      18000,
-      true
-    ),
     getData(`${process.env.WWW}/api/changelog/all`, 18000, false),
   ]);
+
+  const toArray = (object: Object) => Object.values(object).map((item) => item);
+  const statsTranslate =
+    lang === "en" ? toArray(propertiesEN) : toArray(propertiesFR);
+  const relicsSetTranslate =
+    lang === "en" ? toArray(relic_setsEN) : toArray(relic_setsFR);
+  const lightconesTranslate =
+    lang === "en" ? toArray(light_conesEN) : toArray(light_conesFR);
+  const relicsList = lang === "en" ? toArray(relicsEN) : toArray(relicsFR);
+  const eidolonsList =
+    lang === "en" ? toArray(character_ranksEN) : toArray(character_ranksFR);
 
   if (!jsonUid || !resReview) {
     return <div className="text-center mt-10">Chargement en cours ...</div>;
