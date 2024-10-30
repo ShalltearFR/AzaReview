6d057d6ed6912c0c8d00bded70c20352f79bc8a2
ShalltearFR <Jossuha@hotmail.fr>
2024-10-30 15:37:55 +0100

    add slicedText

--- utils/translateBBCode.tsx
+++ utils/translateBBCode.tsx
@@ -2,6 +2,12 @@ import parser from "bbcode-to-react";
 
 const translateBBCode = (text: string) => {
   if (!text) return "";
+  if (text.includes("\n")) {
+    const slicedText = text.split("\n");
+    return slicedText.map((textSliced) => {
+      return <p key={crypto.randomUUID()}>{parser.toReact(textSliced)}</p>;
+    });
+  }
   return parser.toReact(text);
 };
 
