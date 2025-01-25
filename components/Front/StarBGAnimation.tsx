3a8bdaf8b09a2c24db414b28bbe107fadd840c6d
ShalltearFR <Jossuha@hotmail.fr>
2025-01-25 09:42:42 +0100

    remove snow

--- components/Front/StarBGAnimation.tsx
+++ components/Front/StarBGAnimation.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { CDN2 } from "@/utils/cdn";
-import Snowfall from "react-snowfall";
 
 interface StarBGAnimationProps {
   zIndex?: number;
@@ -9,14 +8,6 @@ interface StarBGAnimationProps {
 const StarBGAnimation: React.FC<StarBGAnimationProps> = ({ zIndex = -10 }) => {
   return (
     <div className="absolute w-full h-[calc(100%-205px)]">
-      {/* EFFET NEIGE */}
-      <Snowfall
-        snowflakeCount={75}
-        speed={[0, 1.5]}
-        wind={[-1, 1]}
-        radius={[0.5, 3.5]}
-        style={{ position: "absolute", zIndex: "48", pointerEvents: "none" }}
-      />
       <div
         style={{
           backgroundImage: `url("${CDN2}/img/homepage/stars.svg")`,
