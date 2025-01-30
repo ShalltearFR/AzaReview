30f02ed14f7f13fde730a3f352f053f34db72620
ShalltearFR <Jossuha@hotmail.fr>
2025-01-30 21:11:33 +0100

    add loading page

---
+++ app/hsr-editor/editorChange/loading.tsx
@@ -0,0 +1,9 @@
+import LoadingSpin from "@/components/LoadingSpin";
+
+export default function Loading() {
+  return (
+    <div>
+      <LoadingSpin width="w-24" height="h-24" />
+    </div>
+  );
+}
