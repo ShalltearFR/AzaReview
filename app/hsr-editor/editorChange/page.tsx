5d549daca9324ac2fbf7da09ac19da7bceb0b49e
ShalltearFR <Jossuha@hotmail.fr>
2025-01-30 21:08:24 +0100

    connectDB

--- app/hsr-editor/editorChange/page.tsx
+++ app/hsr-editor/editorChange/page.tsx
@@ -1,5 +1,6 @@
 import EditorChange from "@/models/EditorChange.model";
 import DetailsShow from "./DetailsShow";
+import dbConnect from "@/lib/dbConnect";
 
 interface EditorChangeJSON {
   author: string;
@@ -9,29 +10,35 @@ interface EditorChangeJSON {
 }
 
 const page = async () => {
-  const log = await EditorChange.find({})
-    .sort({ createdAt: -1 })
-    .select("-__v -_id");
+  try {
+    await dbConnect();
+    const log = await EditorChange.find({})
+      .sort({ createdAt: -1 })
+      .select("-__v -_id");
 
-  return (
-    <div className="flex flex-col gap-y-10  mt-5">
-      {log.map((data: EditorChangeJSON, i: number) => (
-        <div
-          key={`dataCard+${i}`}
-          className="bg-gray rounded-lg p-2 w-3/4 mx-auto"
-        >
-          <div className="flex font-bold items-center">
-            <span className="text-xl">{data.author}</span>
-            <span className="ml-auto text-sm">
-              {data.createdAt.toLocaleDateString()}
-            </span>
+    return (
+      <div className="flex flex-col gap-y-10  mt-5">
+        {log.map((data: EditorChangeJSON, i: number) => (
+          <div
+            key={`dataCard+${i}`}
+            className="bg-gray rounded-lg p-2 w-3/4 mx-auto"
+          >
+            <div className="flex font-bold items-center">
+              <span className="text-xl">{data.author}</span>
+              <span className="ml-auto text-sm">
+                {data.createdAt.toLocaleDateString()}
+              </span>
+            </div>
+            <p className="text-lg">{data.comment}</p>
+            <DetailsShow data={data.edit} />
           </div>
-          <p className="text-lg">{data.comment}</p>
-          <DetailsShow data={data.edit} />
-        </div>
-      ))}
-    </div>
-  );
+        ))}
+      </div>
+    );
+  } catch (error) {
+    console.error(error);
+    return <div>Erreur de récuperation de l'editorChange</div>;
+  }
 };
 
 export default page;
