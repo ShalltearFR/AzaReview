83eddaab6795ad80989114de17622ad7644bb862
Shalltear <jossuha@hotmail.fr>
2025-05-25 14:01:28 +0200

    add error 504

---
+++ app/error.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+const error:React.FC = () => {
+  return (
+    <>
+    <div className="text-white text-5xl">Une erreur est survenu, veuillez réessayer plus tard :x</div>
+    <div className="text-white text-5xl mt-5">An error as occured, please retry later :x</div>
+    </>
+  )
+}
+
+export default error
