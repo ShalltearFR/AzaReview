9e5739a544587dccb8aca4cb8575a933c3208ede
Shalltear <jossuha@hotmail.fr>
2026-05-10 11:25:24 +0200

    change CDN

--- utils/cdn.js
+++ utils/cdn.js
@@ -1,3 +1,7 @@
-const CDN = "https://cdn.jsdelivr.net/gh/Mar-7th/StarRailRes@master";
-const CDN2 = "https://cdn.jsdelivr.net/gh/ShalltearFR/AzaReview@main/public";
+/* const CDN = "https://cdn.jsdelivr.net/gh/Mar-7th/StarRailRes@master";
+const CDN2 = "https://cdn.jsdelivr.net/gh/ShalltearFR/AzaReview@main/public"; */
+const CDN =
+  "https://raw.githubusercontent.com/Mar-7th/StarRailRes/refs/heads/master";
+const CDN2 =
+  "https://raw.githubusercontent.com/ShalltearFR/AzaReview/refs/heads/main/public";
 export { CDN, CDN2 };
